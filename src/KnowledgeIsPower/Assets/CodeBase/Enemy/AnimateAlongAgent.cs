b80a40c519fdb6764967dae30c3c9c43118b6433
Egor <a.egordavidov@gmail.com>
2023-01-29 10:32:54 +0300

    feat: add AnimateAlongAgent

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/AnimateAlongAgent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Enemy
+{
+  [RequireComponent(typeof(NavMeshAgent))]
+  [RequireComponent(typeof(EnemyAnimator))]
+  public class AnimateAlongAgent : MonoBehaviour
+  {
+    private const float MinimumVelocity = 0.1f;
+    
+    public EnemyAnimator EnemyAnimator;
+    public NavMeshAgent Agent;
+
+
+    private void Update()
+    {
+      if(ShouldMove())
+        EnemyAnimator.Move(Agent.velocity.magnitude);
+      else
+        EnemyAnimator.StopMoving();
+    }
+
+    private bool ShouldMove() => 
+      Agent.velocity.magnitude > MinimumVelocity && Agent.remainingDistance > Agent.radius;
+  }
+}
