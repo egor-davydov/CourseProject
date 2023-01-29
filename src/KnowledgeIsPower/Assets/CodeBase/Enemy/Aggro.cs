479be76379d5dc95bd518d86fe1f763e30a5bd95
Egor <a.egordavidov@gmail.com>
2023-01-29 11:27:54 +0300

    feat: add Aggro script

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+  public class Aggro : MonoBehaviour
+  {
+    public TriggerObserver TriggerObserver;
+    public AgentMoveToPlayer Follow;
+
+    private void Start()
+    {
+      TriggerObserver.TriggerEnter += TriggerEnter;
+      TriggerObserver.TriggerExit += TriggerExit;
+      
+      Follow.enabled = false;
+    }
+
+    private void TriggerEnter(Collider obj)
+    {
+      SwitchFollowOn();
+    }
+
+    private void TriggerExit(Collider obj)
+    {
+      SwitchFollowOff();
+    }
+
+    private bool SwitchFollowOn() => 
+      Follow.enabled = true;
+
+    private bool SwitchFollowOff() =>
+      Follow.enabled = false;
+  }
+}
