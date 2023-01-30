8efcdcb02048e688086e262ec6c7947424b898c7
Egor <a.egordavidov@gmail.com>
2023-01-30 17:20:46 +0300

    feat: add HeroDeath script

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Hero/HeroDeath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Hero
+{
+  [RequireComponent(typeof(HeroHealth))]
+  public class HeroDeath : MonoBehaviour
+  {
+    public HeroHealth Health;
+    public HeroMove Move;
+    public GameObject DeathFx;
+    public HeroAnimator Animator;
+    
+    private bool _isDead;
+
+    private void Start() => 
+      Health.HealthChanged += HealthChanged;
+
+    private void OnDestroy() => 
+      Health.HealthChanged -= HealthChanged;
+
+    private void HealthChanged()
+    {
+      if (!_isDead && Health.Current <= 0)
+        Die();
+    }
+
+    private void Die()
+    {
+      _isDead = true;
+      
+      Move.enabled = false;
+      Animator.PlayDeath();
+      Instantiate(DeathFx, transform.position, Quaternion.identity);
+    }
+  }
+}
