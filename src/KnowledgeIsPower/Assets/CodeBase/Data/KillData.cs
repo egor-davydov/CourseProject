5dbd53e33e5ffa858fd6df9fb1f22745d9af8681
Egor <a.egordavidov@gmail.com>
2023-02-06 22:09:35 +0300

    feat: make EnemySpawner implements ISavedProgress

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Data/KillData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CodeBase.Data
+{
+  public class KillData
+  {
+    public List<string> ClearedSpawners = new List<string>();
+  }
+}
--- src/KnowledgeIsPower/Assets/CodeBase/Logic/EnemySpawner.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Logic/EnemySpawner.cs
@@ -1,10 +1,39 @@
-﻿using CodeBase.StaticData;
+﻿using System;
+using CodeBase.Data;
+using CodeBase.Infrastructure.Services.PersistentProgress;
+using CodeBase.StaticData;
 using UnityEngine;
 
 namespace CodeBase.Logic
 {
-  public class EnemySpawner : MonoBehaviour
+  public class EnemySpawner : MonoBehaviour, ISavedProgress
   {
     public MonsterTypeId MonsterTypeId;
+    public bool Slayed;
+
+    private string _id;
+
+    private void Awake()
+    {
+      _id = GetComponent<UniqueId>().Id;
+    }
+
+    public void LoadProgress(PlayerProgress progress)
+    {
+      if (progress.KillData.ClearedSpawners.Contains(_id))
+        Slayed = true;
+      else
+        Spawn();
+    }
+
+    public void UpdateProgress(PlayerProgress progress)
+    {
+      if (Slayed)
+        progress.KillData.ClearedSpawners.Add(_id);
+    }
+
+    private void Spawn()
+    {
+    }
   }
 }
