5d5a54e6a28a4ebaa2fd5c5f884e2f3dee4a788c
Egor <a.egordavidov@gmail.com>
2023-02-06 23:03:52 +0300

    feat: add EnemySpawner tag

--- src/KnowledgeIsPower/Assets/CodeBase/Data/KillData.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Data/KillData.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CodeBase.Data
 {
+  [Serializable]
   public class KillData
   {
     public List<string> ClearedSpawners = new List<string>();
