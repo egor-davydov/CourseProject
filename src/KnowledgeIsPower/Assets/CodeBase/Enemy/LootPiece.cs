04a293763886db508e899692b51e987f6c3c581b
Egor <a.egordavidov@gmail.com>
2023-02-15 11:47:16 +0300

    feat: add pickup visualization to script

--- src/KnowledgeIsPower/Assets/CodeBase/Enemy/LootPiece.cs
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/LootPiece.cs
@@ -1,26 +1,28 @@
-﻿using CodeBase.Data;
+﻿using System.Collections;
+using CodeBase.Data;
+using TMPro;
 using UnityEngine;
 
 namespace CodeBase.Enemy
 {
   public class LootPiece : MonoBehaviour
   {
+    public GameObject Skull;
+    public GameObject PickupFx;
+    public GameObject PickupPopup;
+    public TextMeshPro LootText;
+    
     private Loot _loot;
     private bool _picked;
     private WorldData _worldData;
 
-    public void Construct(WorldData worldData)
-    {
+    public void Construct(WorldData worldData) => 
       _worldData = worldData;
-    }
 
-    public void Initialize(Loot loot)
-    {
+    public void Initialize(Loot loot) => 
       _loot = loot;
-    }
 
-    private void OnTriggerEnter(Collider other) => 
-      PickUp();
+    private void OnTriggerEnter(Collider other) => PickUp();
 
     private void PickUp()
     {
@@ -29,7 +31,33 @@ namespace CodeBase.Enemy
       
       _picked = true;
 
+      UpdateWorldData();
+      HideSkull();
+      ShowText();
+      PlayPickupFx();
+      StartCoroutine(DestroyTimer());
+    }
+
+    private void UpdateWorldData() => 
       _worldData.LootData.Collect(_loot);
+
+    private void PlayPickupFx() => 
+      Instantiate(PickupFx, transform.position, Quaternion.identity);
+
+    private void HideSkull() => 
+      Skull.SetActive(false);
+
+    private void ShowText()
+    {
+      LootText.text = $"{_loot.Value}";
+      PickupPopup.SetActive(true);
+    }
+
+    private IEnumerator DestroyTimer()
+    {
+      yield return new WaitForSeconds(1.5f);
+      
+      Destroy(gameObject);
     }
   }
 }
